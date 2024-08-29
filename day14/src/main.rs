bd716d31c403b2b2c9fc4657b762cd39b0bf0201
TorinM <torinmay@gmail.com>
2024-08-29 21:20:00 +0900

    Correct answer for in problem statement example

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -25,8 +25,84 @@ fn get_input(file_name:&str) -> Vec<Vec<char>> {
     result
 }
 
-fn main() {
-    let input = get_input("input.txt");
+fn move_rock_north(platform: &mut Vec<Vec<char>>, row: usize, col: usize) {
+    // Algorithm:
+    // 1. Check if the rock can be moved up
+    // 2. If it can, move it up
+    // 3. If it can't, do nothing
+    // 4. Repeat until rock is stopped 
+
+    let mut row = row;
+    let col = col;
+
+    while row > 0 {
+        if platform[row - 1][col] == '.' {
+            platform[row - 1][col] = 'O';
+            platform[row][col] = '.';
+            row -= 1;
+        } else {
+            break;
+        }
+    }
+}
+
+// Tilt the platform to the north so all chars fall to top in accordance with problem statement
+fn tilt_north(platform: &mut Vec<Vec<char>>) {
+    // Algorithm:
+    // 1. Iterate over each row
+    // 2. Iterate over each item in row, if item is a O, move it up until it hits a wall or another
+    //    O or the top of the platform
+    // 3. Repeat until all items are moved up
     
-    println!("{:?}", input);
+    let platform_clone = platform.clone();
+    let mut row_num: usize = 0;
+    
+    for row in platform_clone {
+        let mut col_num: usize = 0;
+        for i in row {
+            if i == 'O' {
+               move_rock_north(platform, row_num, col_num);
+            }
+            col_num += 1;
+        }
+
+        row_num += 1;
+    }
+}
+
+fn count_load(platform: &Vec<Vec<char>>) -> usize {
+    let mut total_load = 0;
+
+    let mut row_num: usize = platform.len();
+
+    for row in platform.clone() {
+        let mut row_load = 0;
+        for i in row {
+            if i == 'O' {
+                row_load += row_num;
+            }
+        }
+        total_load += row_load;
+        row_num -= 1;
+    }
+
+    total_load
+}
+
+fn print_platform(platform: &Vec<Vec<char>>) {
+    for row in platform {
+        println!("{:?}", row);
+    }
+    println!();
+}
+
+fn main() {
+    let mut platform = get_input("input.txt");
+    print_platform(&platform);
+
+    tilt_north(&mut platform);
+    
+    print_platform(&platform);
+
+    println!("Total load: {}", count_load(&platform));
 }
